55c1a6f1e1173d1f4b17f3322e4950f6d81304a9
Luca Angioloni <lucaangioloni@gmail.com>
2025-02-13 13:21:39 +0100

    Enhance get_dir_content function to support multiple patterns for filtering and excluding files

--- get_files_list/get_dir_content.py
+++ get_files_list/get_dir_content.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import os
 import fnmatch
 from typing import Generator
@@ -17,7 +18,7 @@ def _get_dir_content(path, include_folders, recursive):
             yield entry_with_path
 
 
-def get_dir_content(path: str, include_folders: bool = False, recursive: bool = True, prepend_folder_name: bool = True, pattern: str = None, exclude_pattern: str = None) -> Generator[str, None, None]:
+def get_dir_content(path: str, include_folders: bool = False, recursive: bool = True, prepend_folder_name: bool = True, pattern: str | list | None = None, exclude_pattern: str | list | None = None) -> Generator[str, None, None]:
     """Function that recursively gets files that match a pattern in a given directory.
 
     Args:
@@ -25,14 +26,18 @@ def get_dir_content(path: str, include_folders: bool = False, recursive: bool = 
         include_folders (bool, optional): Return not only files but folders as well. Defaults to False.
         recursive (bool, optional): If False, only files directly under the given path are returned; otherwise search in subfolders recursively. Defaults to True.
         prepend_folder_name (bool, optional): If false do not add the `path` to the returned file paths. Defaults to True.
-        pattern (_type_, optional): A Unix Shell like pattern used to filter files (For exaple "*.jpg" would only return files ending in `.jpg`). Defaults to None.
-        exclude_pattern (_type_, optional): A Unix Shell like pattern used to exclude files (For exaple "*.jpg" would exclude all files ending in `.jpg`). Defaults to None.
+        pattern (str | list, optional): A Unix Shell like pattern used to filter files (For exaple "*.jpg" would only return files ending in `.jpg`). Defaults to None. You can also pass a list of patterns to match and if any the path will be returned.
+        exclude_pattern (str | list, optional): A Unix Shell like pattern used to exclude files (For exaple "*.jpg" would exclude all files ending in `.jpg`). Defaults to None. You can also pass a list of patterns to exclude and if any the path will be excluded.
 
     Yields:
         str: A file path.
     """
     path_len = len(path) + (len(os.path.sep)
                             if not path.endswith(os.path.sep) else 0)
+    if isinstance(pattern, str):
+        pattern = [pattern]
+    if isinstance(exclude_pattern, str):
+        exclude_pattern = [exclude_pattern]
     for item in _get_dir_content(path, include_folders, recursive):
-        if (pattern is None or fnmatch.fnmatch(item, pattern)) and (exclude_pattern is None or not fnmatch.fnmatch(item, exclude_pattern)):
+        if (pattern is None or any([fnmatch.fnmatch(item, p) for p in pattern])) and (exclude_pattern is None or not any([fnmatch.fnmatch(item, ep) for ep in exclude_pattern])):
             yield item if prepend_folder_name else item[path_len:]
