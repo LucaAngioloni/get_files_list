e5d7f2427a234d7a92b40633cd12f30f757aac00
Luca Angioloni <lucaangioloni@gmail.com>
2025-12-31 01:55:39 +0100

    refactor: Update deployment script to use python -m build and remove deprecated setup.py commands

--- setup.py
+++ setup.py
@@ -44,7 +44,6 @@ setup(
         "Intended Audience :: Developers",
         "Intended Audience :: Other Audience",
         "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Topic :: Utilities",
